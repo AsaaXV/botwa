47906d7492bba17c680fe4adeeb99264b72299c4
AndyJs <88842740+andyjavadams@users.noreply.github.com>
2021-12-21 06:01:03 +0700

    Bamru

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,72 +1,35 @@
-/*let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-conn.sendContact(m.chat, '6285795431803', 'Andy', m)
-conn.reply(m.chat, `@${global.owner[0]}
-`.trim(), m, { contextIfo: { mentionedJid: [global.owner[0] + '@s.whatsapp.net'] } })
-conn.reply(m.chat, `wa.me/6285795431803`, m)
-    }*/
-const { MessageType } = require('@adiwajshing/baileys')
-const PhoneNumber = require('awesome-phonenumber')
-async function handler(m) {
-  let name = 'Andy'
-  number = owner[0].replace(/[^0-9]/g, '')
-  let njid = number + '@s.whatsapp.net'
-  let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
-
-  let name2 = 'Andy Bot'
-  number2 = owner[1].replace(/[^0-9]/g, '')
-  let njid2 = number2 + '@s.whatsapp.net'
-  let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-  
-  let name3 = 'Bang jamal'
-  number3 = owner[2].replace(/[^0-9]/g, '')
-  let njid3 = number3 + '@s.whatsapp.net'
-  let onW3 = await this.isOnWhatsApp(njid3) || { isBusiness: false }
-  
-    this.sendMessage(m.chat, {
-    contacts: [{
-      displayname: name, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name.replace(/\n/g, '\\n')};;;
-FN:${name.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}${onW.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid].vname || this.getName(njid)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    },
-    {
-      displayname: name, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name3.replace(/\n/g, '\\n')};;;
-FN:${name3.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number3}:${PhoneNumber('+' + number3).getNumber('international')}${onW3.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid3].vname || this.getName(njid3)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    },
-    {
-      displayname: name2, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name2.replace(/\n/g, '\\n')};;;
-FN:${name2.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number2}:${PhoneNumber('+' + number2).getNumber('international')}${onW2.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid2].vname || this.getName(njid2)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid2)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }]
-  }, MessageType.contactsArray, { quoted: m })
-  m.reply(`@${owner[0]}\n@${owner[1]}\n@${owner[2]}`)
+let fs = require ('fs')
+let handler = async function (m) {
+  const freply = {
+  key: {
+  participant: '0@s.whatsapp.net',
+  remoteJid: 'status@broadcast'
+ },
+ message: {
+  imageMessage: {
+   caption: `${pickRandom(['*Wis*','aku sayang kamu','*xixi*','*cans cuk*','*kawaii*','*hemhe*'])}`,
+      jpegThumbnail: fs.readFileSync(`./cewe/${pickRandom(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'])}.jpg`)
+  }
+ }
 }
-  
-handler.command = /^owner|creator$/i
+let list = []
+  for (let i of owner.map(v => v + '@s.whatsapp.net')) {
+  list.push({
+            "displayName": this.getName(i),
+            "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+          })
+  }
+        await this.sendMessage(m.chat, {
+        "displayName": `${list.length} Contact`,
+        "contacts": list 
+        }, 'contactsArrayMessage', { quoted: freply, sendEphemeral: true, contextInfo: { forwardingScore: 135, isForwarded: true }})
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
 
 module.exports = handler
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
